4565f3b5f6732e7879f43bac0b7c0f94be48a66a
yossefsabry <yossefsabry66@gmail.com>
2024-04-28 08:02:19 -0400

    adding new update  for spinner and convet to promise
    converting the main function to promise function covert from (async, await) and adding new spinner
    for loading

--- js/script.js
+++ js/script.js
@@ -3,47 +3,61 @@
 
 
 let city = 'Cairo'
-var apiKey = "ebc504ae8fffd7cbc062f1ac72525a11";
-var apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}`
+const apiKey = "ebc504ae8fffd7cbc062f1ac72525a11";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}`
 
-async function checkWeather() {
+// lsp suggest convert ot async func
+function checkWeather() {
+  return new Promise ((reslove, reject) => {
     try {
-        const response = await fetch(apiUrl + `&appid=${apiKey}`);
-        var data = await response.json();
-
-        // Add the fade-in class to the target elements
-        const tempElement = document.getElementById("temp");
-        tempElement.textContent = data.main.temp + "°";
-        tempElement.classList.add("fade-in");
-
-        const nameElement = document.querySelector(".name h1");
-        nameElement.textContent = data.name;
-        nameElement.classList.add("fade-in");
-
-        const weatherElement = document.querySelector(".weather span");
-        weatherElement.textContent = data.weather[0].description;
-        weatherElement.classList.add("fade-in");
-
-        const countryElement = document.getElementById("country");
-        countryElement.textContent = data.sys.country;
-        countryElement.classList.add("fade-in");
-
-        const humidityElement = document.getElementById("humidity");
-        humidityElement.textContent = data.main.humidity + "%";
-        humidityElement.classList.add("fade-in");
-
-        const windElement = document.getElementById("wind_speed");
-        windElement.textContent = data.wind.speed + "Km/h";
-        windElement.classList.add("fade-in");
-
-        const timezoneElement = document.getElementById("timezone");
-        timezoneElement.textContent = data.timezone;
-        timezoneElement.classList.add("fade-in");
-
-
-    } catch (error) {
-        console.log(error);
+      const response =  fetch(apiUrl + `&appid=${apiKey}`);
+      reslove(response);
+    } catch (e) {
+      reject(`error happend: ${e}`)
     }
+  }, (e) => console.log(e))
+
+    .then((response) => {
+      return new Promise((reslove, reject) => {
+        try {
+          const data = response.json();
+          reslove(data)
+        }catch(e) {
+          reject(e)
+        }
+      }, (e) => console.log(e))
+          // Add the fade-in class to the target elements
+        .then((data) => {
+          const tempElement = document.getElementById("temp");
+          tempElement.textContent = data.main.temp + "°";
+          tempElement.classList.add("fade-in");
+
+          const nameElement = document.querySelector(".name h1");
+          nameElement.textContent = data.name;
+          nameElement.classList.add("fade-in");
+
+          const weatherElement = document.querySelector(".weather span");
+          weatherElement.textContent = data.weather[0].description;
+          weatherElement.classList.add("fade-in");
+
+          const countryElement = document.getElementById("country");
+          countryElement.textContent = data.sys.country;
+          countryElement.classList.add("fade-in");
+
+          const humidityElement = document.getElementById("humidity");
+          humidityElement.textContent = data.main.humidity + "%";
+          humidityElement.classList.add("fade-in");
+
+          const windElement = document.getElementById("wind_speed");
+          windElement.textContent = data.wind.speed + "Km/h";
+          windElement.classList.add("fade-in");
+
+          const timezoneElement = document.getElementById("timezone");
+          timezoneElement.textContent = data.timezone;
+          timezoneElement.classList.add("fade-in");
+
+        })
+    })
 }
 checkWeather()
 
@@ -61,13 +75,13 @@ items.forEach(item => {
 
 
 // search for city 
-var input = document.querySelector(".search #input_city");
-var submit = document.querySelector("#submit");
+const input = document.querySelector(".search #input_city");
+const submit = document.querySelector("#submit");
 
 submit.addEventListener("click", async function (event) {
     event.preventDefault(); // Prevent default form submission behavior
 
-    let value = input.value;
+    const value = input.value;
     city = value;
     apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
     await checkWeather();
@@ -79,12 +93,12 @@ submit.addEventListener("click", async function (event) {
 //! time
 
 function displayDateTime() {
-    var today = new Date()
-    var day = today.getDate();
-    var month = today.getMonth();
-    var fullYear = today.getFullYear();
-    var time = today.toLocaleTimeString()
-    document.querySelector(".time").innerHTML = day + ' - ' + month + ' - ' + fullYear + ' , ' + time;
+    const today = new Date()
+    const day = today.getDate();
+    const month = today.getMonth();
+    const fullYear = today.getFullYear();
+    const time = today.toLocaleTimeString()
+    document.querySelector(".time").innerHTML = day + '-' + month + '-' + fullYear + ' , ' + time;
 }
 
 // Call the function initially to display the date and time
@@ -95,8 +109,8 @@ setInterval(displayDateTime, 1000);
 
 
 // show menu
-let menuBars = document.getElementById("menu_bar")
-let menu = document.querySelector("header .container .info")
+const menuBars = document.getElementById("menu_bar")
+const menu = document.querySelector("header .container .info")
 let menuOption = "hidden"
 
 menuBars.onclick = () => {
